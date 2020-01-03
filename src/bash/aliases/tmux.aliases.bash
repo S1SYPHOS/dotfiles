22f297291015c8090cf8d7e5858486e625eec203
Martin Folkers <hello@twobrain.io>
2020-01-03 13:19:30 +0100

    Adding dotfile for tmux

--- src/bash/aliases/tmux.aliases.bash
+++ src/bash/aliases/tmux.aliases.bash
@@ -1,7 +1,7 @@
 ## TMUX ##
 
 if command -v tmux >/dev/null 2>&1; then
-    # alias tmux="TERM=xterm-256color tmux"
+    alias tmux='TERM=xterm-256color tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf'
     alias txl="tmux ls"
     alias txn="tmux new -s"
     alias txa="tmux a -t"
