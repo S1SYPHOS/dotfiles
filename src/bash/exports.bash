980378117487b9ba84f989914a02ce093ffd5702
Martin Folkers <hello@twobrain.io>
2020-01-13 21:53:27 +0100

    Changing man colors to match 'Nord' theme

--- src/bash/exports.bash
+++ src/bash/exports.bash
@@ -95,12 +95,12 @@ export GROFF_NO_SGR="1"
 
 man() {
 	env \
-        LESS_TERMCAP_mb=$'\e[1;31m' \
-        LESS_TERMCAP_md=$'\e[1;36m' \
-        LESS_TERMCAP_so=$'\e[01;44;37m' \
-        LESS_TERMCAP_us=$'\e[01;37m' \
-        LESS_TERMCAP_me=$'\e[0m' \
-        LESS_TERMCAP_se=$'\e[0m' \
-        LESS_TERMCAP_ue=$'\e[0m' \
+        LESS_TERMCAP_mb=$'\E[1;34m' \
+        LESS_TERMCAP_md=$'\E[1;34m' \
+        LESS_TERMCAP_me=$'\E[0m' \
+        LESS_TERMCAP_so=$'\E[01;35m' \
+        LESS_TERMCAP_se=$'\E[0m' \
+        LESS_TERMCAP_us=$'\E[1;32m' \
+        LESS_TERMCAP_ue=$'\E[0m' \
 	    man "$@"
 }
